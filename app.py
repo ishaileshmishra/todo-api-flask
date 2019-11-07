d888fc0314fbcc60e1f8902ca22ba2dbe1e45254
mshaileshr@gmail.com <Yes@383840>
2019-11-07 18:28:43 +0530

    webapp using flask-python

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def get_tasks():
     return jsonify({'tasks': tasks})
 
 
+# this is to show error in valid formate
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
